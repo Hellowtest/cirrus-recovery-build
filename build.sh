e2dddbc76394c3a43455799416c4540984977feb
Hellowtest <127306591+Hellowtest@users.noreply.github.com>
2025-12-31 16:08:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ log "Building TWRP..."
 export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
 lunch twrp_a05m-ap3a-eng
-lunch twrp_a05m-ap4a-eng
+#lunch twrp_a05m-ap4a-eng
 
 mka recoveryimage -j"$(nproc --all)"
 
